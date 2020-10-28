92200c6ce5f6133f9c6c901cf60aca07d952b1f1
Ryan Nelson <nelsonr@email.arizona.edu>
2020-10-28 07:48:12 -0700

    Defines main() function so that comic range is more visible. Changes general comments to this particular code (project description moved to README

--- asw.py
+++ asw.py
@@ -3,35 +3,20 @@
 Created on Tue Oct 20 11:21:20 2020
 @author: rnelson2
 
-This is the start of a project that I first thought of just under 5 years ago,
-on May 27, 2005, just a few days before the final A Softer World comic.
-
-The spirit of the idea is to look for any sort of pattern in how sentences are
-broken up between panels in A Softer World comics, relative to the structure
-of the sentences. Because I had often found the breaks to be counter-intuitive,
-I might expect a high occurence of the breaks being within major syntactic
-constituents/phrases, rather than at their boundaries.
-
-***As a start to this project, this code downloads and saves all ASW comics.***
-
-Next steps would be:
-
-Use OCR to extract the text from all of the comics, using the comic panels as 
-a basis to annotate the breaks.
-
-Use NLPT and perhaps statistical packages to look for patterns, if any, within
-those break.
-
-I also want to be able to extract the alt-text (which is
-'title') and match it with each comic's text and
-filename. In the interest of making this more aligned with the CS50, I suppose
-I could throw it all in an SQL database with a search feature. Maybe categorize
-them there by whatever type of syntactic breaks.
+This section of code defines a function to download and save
+all A Softer World comics from the website, utilizing the
+Beautiful Soup package.
 
 """
 
 import bs4 #beautifulsoup4
 import requests
+
+# UPDATE COMIC RANGE IN FUNCTION CALL
+def main():
+    for i in range(700,705):
+        save_comic(i)
+
 
 def save_comic(n):
     
@@ -56,6 +41,5 @@ def save_comic(n):
     with open(f'comics/{comicnumber:04d}_{filename}', 'wb') as img:
         img.write(imgurl.content)
 
-for i in range(700,705):
-    save_comic(i)
+main()
 
