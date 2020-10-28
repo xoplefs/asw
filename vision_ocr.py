10a22e2244ba01b3a8bc23cfa6c0d8b90ab0d708
Ryan Nelson <nelsonr@email.arizona.edu>
2020-10-28 07:25:07 -0700

    Adds comment describing plan to return string output

--- vision_ocr.py
+++ vision_ocr.py
@@ -7,10 +7,10 @@ Created on Tue Oct 27 12:24:39 2020
 Testing the Google Cloud Vision API for OCR
 """
 
-path = "comics/0001_myparents.jpg"
-
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/xople/Documents/ASW/nomadic-zoo-293819-8ccfdaa58681.json"
+
+path = "comics/0001_myparents.jpg"
 
 
 #def detect_text(path):
@@ -28,7 +28,7 @@ image = types.Image(content=content)
 response = client.text_detection(image=image)
 texts = response.text_annotations
 
-print(type(texts))
+#print(type(texts))
 
 print('Texts:')
 
@@ -39,6 +39,14 @@ for text in texts:
 #                 for vertex in text.bounding_poly.vertices])
 
 #     print('bounds: {}'.format(','.join(vertices)))
+
+'''
+Within 'texts' are objects for 'description', which is the read text,
+and appears to be separated by word, and also includes new lines.
+These are an EntityAnnotation type, but can be converted to str()
+
+So I think I can just convert to string and return the result to the main function
+'''
 
 if response.error.message:
     raise Exception(
