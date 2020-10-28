a16e8ba885d32531e10fb700f08978c986ed891a
Ryan Nelson <nelsonr@email.arizona.edu>
2020-10-28 06:08:08 -0700

    First working OCR with API key. Still need to figure out more on how the output works.

--- vision_ocr.py
+++ vision_ocr.py
@@ -9,31 +9,35 @@ Testing the Google Cloud Vision API for OCR
 
 
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/rnelson2/asw/nomadic-zoo-293819-8ccfdaa58681.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/xople/Documents/ASW/nomadic-zoo-293819-8ccfdaa58681.json"
 
 
 def detect_text(path):
     """Detects text in the file."""
     from google.cloud import vision
+    from google.cloud.vision import types
     import io
     client = vision.ImageAnnotatorClient()
 
     with io.open(path, 'rb') as image_file:
         content = image_file.read()
 
-    image = vision.Image(content=content)
+    image = types.Image(content=content)
 
     response = client.text_detection(image=image)
     texts = response.text_annotations
+    
+    print(type(texts))
+    
     print('Texts:')
 
     for text in texts:
         print('\n"{}"'.format(text.description))
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
+    #     vertices = (['({},{})'.format(vertex.x, vertex.y)
+    #                 for vertex in text.bounding_poly.vertices])
 
-        print('bounds: {}'.format(','.join(vertices)))
+    #     print('bounds: {}'.format(','.join(vertices)))
 
     if response.error.message:
         raise Exception(
